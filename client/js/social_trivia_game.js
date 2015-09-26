a6429535a19ac841e9a10cfb176cc38092a99bb9
Ashay Sheth <ashay.sheth@gmail.com>
2015-09-26 18:42:25 -0400

    can join games

--- client/js/social_trivia_game.js
+++ client/js/social_trivia_game.js
@@ -21,6 +21,8 @@ if (Meteor.isClient) {
           Meteor.loginWithFacebook({}, function(err){
               if (err) {
                   throw new Meteor.Error("Facebook login failed");
+              } else {
+                Router.go('/join/');
               }
           });
       },
@@ -33,4 +35,35 @@ if (Meteor.isClient) {
           })
       }
   });
+
+  Template.join.events({
+      "submit #join-game": function (event) {
+        event.preventDefault();
+        var gameToJoin = event.target.gameid.value;
+        if (Games.findOne({_id: gameToJoin})) {
+          if (Players.findOne({pID: Meteor.user()._id})) {
+            var id = Players.findOne({pID: Meteor.user()._id})._id;
+            Players.update(id, {
+                    $set: {gameID: gameToJoin}
+                  });
+          } else {
+            Players.insert({
+                    pID: Meteor.user()._id,
+                    gameID: gameToJoin,
+                    name: Meteor.user().profile.name,
+                    createdAt: new Date() // current time
+                  });            
+          }
+          Router.go('/joined/');          
+        } else {
+          console.log("error");
+        }
+      }
+    });
+
+  Template.game.helpers({
+      players: function () {
+        return Players.find({gameID: this._id});
+      }
+    });
 }
