40756fd43ee9e5f949a86240a42c7cac10bc46c5
ciyengar3 <ciyengar3@gmail.com>
2015-09-26 21:42:09 -0400

    Added Semicolons
    yup

--- client/js/landing.js
+++ client/js/landing.js
@@ -1,8 +1,8 @@
   Template.landing.events({
     'click #create_game': function () {
       event.preventDefault();
-      var shortId = Math.random() * 1000
-      shortId = Math.round(shortId)
+      var shortId = Math.random() * 1000;
+      shortId = Math.round(shortId);
       var id = Games.insert({
         createdAt: new Date(),
         newId: shortId 
