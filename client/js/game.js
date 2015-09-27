8f1e148ae35dfbd773d9b5156c8e735277bbce96
nbawa95 <nbawa95@gmail.com>
2015-09-26 21:40:20 -0400

    Added common question screen

--- client/js/game.js
+++ client/js/game.js
@@ -6,3 +6,23 @@
       	return Players.find({gameID: this.newId}).count() > 1;
       }
     });
+
+ Template.game.events({
+ 	'click #startButton': function () {
+      event.preventDefault();
+      var array = Questions.find().fetch();
+      var randomIndex = Math.floor( Math.random() * array.length);
+      var id = Rounds.insert({
+      	gameID: this._id,
+        createdAt: new Date(), 
+        questionID: array[randomIndex]._id
+      });
+      Games.update(this._id, {
+        $set: {
+        	roundID: id,
+        	gameStarted: true
+        }
+      });
+    Router.go('/question/' + array[randomIndex]._id);
+ }
+});
