b8d641899abdcd7cd3952597c07cf95f9642d493
ciyengar3 <ciyengar3@gmail.com>
2015-09-26 20:13:14 -0400

    Added Start Button

--- client/js/game.js
+++ client/js/game.js
@@ -1,5 +1,8 @@
   Template.game.helpers({
       players: function () {
         return Players.find({gameID: this.newId});
+      },
+      isTwo: function () {
+      	return Players.find({gameID: this.newId}).count() > 1;
       }
     });
